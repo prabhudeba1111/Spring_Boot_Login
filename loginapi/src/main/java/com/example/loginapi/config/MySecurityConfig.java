a673d15f383ac68f382601230e049a13b132040e
prabhudeba1111 <137163103+prabhudeba1111@users.noreply.github.com>
2023-06-20 15:00:00 +0530

    Update MySecurityConfig.java

--- loginapi/src/main/java/com/example/loginapi/config/MySecurityConfig.java
+++ loginapi/src/main/java/com/example/loginapi/config/MySecurityConfig.java
@@ -2,32 +2,16 @@ package com.example.loginapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class MySecurityConfig {
-
-    @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user1 = User.builder().username("user1").password(passwordEncoder().encode("jkl")).roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(user1);
-    }
+    
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
-        return builder.getAuthenticationManager();
     }
 }

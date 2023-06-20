691794d0435446c4cbb6cb7b738f8d9deecac1e4
prabhudeba1111 <137163103+prabhudeba1111@users.noreply.github.com>
2023-06-20 15:13:10 +0530

    Update UserDetailsService.java

--- loginapi/src/main/java/com/example/loginapi/service/UserDetailsService.java
+++ loginapi/src/main/java/com/example/loginapi/service/UserDetailsService.java
@@ -1,8 +1,5 @@
 package com.example.loginapi.service;
 
-import com.example.loginapi.entity.User;
-import com.example.loginapi.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;

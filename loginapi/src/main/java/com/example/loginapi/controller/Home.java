a36f8933677e0220073e8b509bb2e3bfbdeba9a5
prabhudeba1111 <137163103+prabhudeba1111@users.noreply.github.com>
2023-06-20 15:07:39 +0530

    Update Home.java

--- loginapi/src/main/java/com/example/loginapi/controller/Home.java
+++ loginapi/src/main/java/com/example/loginapi/controller/Home.java
@@ -1,25 +1,13 @@
 package com.example.loginapi.controller;
 
-import com.example.loginapi.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Home {
-
-    @Autowired
-    UserDetailsService userDetailsService;
-
+public class Dashboard {
     @RequestMapping("/welcome")
     public String welcome(){
         String s = "This is a private page";
         return s;
     }
 }
-
-
-
-
-

f49ad79230f4746b3498c590acda06fdbe13ffc9
prabhudeba1111 <137163103+prabhudeba1111@users.noreply.github.com>
2023-07-06 12:32:17 +0530

    Delete loginapi/src/main/java/com/example/loginapi/service directory

--- loginapi/src/main/java/com/example/loginapi/service/UserDetailsService.java
+++
@@ -1,8 +0,0 @@
-package com.example.loginapi.service;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface UserDetailsService {
-    UserDetails loadUserByUsername(String email) throws UsernameNotFoundException;
-}
